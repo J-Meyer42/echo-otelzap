6323bd73a5330bc39740d83593ab17f75c25cf85
J-Meyer42 <39696570+J-Meyer42@users.noreply.github.com>
2023-06-06 07:16:23 -0700

    New features and configurable options.
    feat: Added OpenTelemetry support via the Uptrace otelzap library.
    feat: Added support for logging user defined custom fields.
    feat: Skipper (contributed by thzois)

--- logger.go
+++ logger.go
@@ -5,12 +5,45 @@ import (
 	"time"
 
 	"github.com/labstack/echo/v4"
+	"github.com/uptrace/opentelemetry-go-extra/otelzap"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
+type (
+	Skipper func(c echo.Context) bool
+
+	// ZapLoggerConfig defines the config for ZapLogger middleware
+	ZapLoggerConfig struct {
+		Skipper         Skipper
+		CustomFieldFunc func(c echo.Context) []zapcore.Field
+	}
+)
+
+var (
+	// DefaultZapLoggerConfig is the default ZapLogger middleware config.
+	DefaultZapLoggerConfig = ZapLoggerConfig{
+		Skipper: DefaultSkipper,
+	}
+)
+
+// DefaultSkipper returns false which processes the middleware
+func DefaultSkipper(echo.Context) bool {
+	return false
+}
+
 // ZapLogger is a middleware and zap to provide an "access log" like logging for each request.
 func ZapLogger(log *zap.Logger) echo.MiddlewareFunc {
+	return ZapLoggerWithConfig(log, DefaultZapLoggerConfig)
+}
+
+// OtelZapLogger is a middleware and zap to provide an "access log" like logging and opentelemetry support for each request.
+func OtelZapLogger(log *otelzap.Logger) echo.MiddlewareFunc {
+	return OtelZapLoggerWithConfig(log, DefaultZapLoggerConfig)
+}
+
+// ZapLoggerWithConfig is a middleware (with configuration) and zap to provide an "access log" like logging for each request.
+func ZapLoggerWithConfig(log *zap.Logger, config ZapLoggerConfig) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			start := time.Now()
@@ -38,6 +71,72 @@ func ZapLogger(log *zap.Logger) echo.MiddlewareFunc {
 				id = res.Header().Get(echo.HeaderXRequestID)
 			}
 			fields = append(fields, zap.String("request_id", id))
+
+			// Append custom logger fields if provided
+			if config.CustomFieldFunc != nil {
+				fields = append(fields, config.CustomFieldFunc(c)...)
+			}
+
+			n := res.Status
+			switch {
+			case n >= 500:
+				log.With(zap.Error(err)).Error("Server error", fields...)
+			case n >= 400:
+				log.With(zap.Error(err)).Warn("Client error", fields...)
+			case n >= 300:
+				log.Info("Redirection", fields...)
+			default:
+				log.Info("Success", fields...)
+			}
+
+			return nil
+		}
+	}
+}
+
+// OtelZapLoggerWithConfig is a middleware (with configuration) and otelzap to provide an "access log" like logging and opentelemetry support for each request.
+func OtelZapLoggerWithConfig(log *otelzap.Logger, config ZapLoggerConfig) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		// Defaults
+		if config.Skipper == nil {
+			config.Skipper = DefaultZapLoggerConfig.Skipper
+		}
+
+		return func(c echo.Context) error {
+			if config.Skipper(c) {
+				return next(c)
+			}
+
+			start := time.Now()
+
+			err := next(c)
+			if err != nil {
+				c.Error(err)
+			}
+
+			req := c.Request()
+			res := c.Response()
+
+			fields := []zapcore.Field{
+				zap.String("remote_ip", c.RealIP()),
+				zap.String("latency", time.Since(start).String()),
+				zap.String("host", req.Host),
+				zap.String("request", fmt.Sprintf("%s %s", req.Method, req.RequestURI)),
+				zap.Int("status", res.Status),
+				zap.Int64("size", res.Size),
+				zap.String("user_agent", req.UserAgent()),
+			}
+
+			id := req.Header.Get(echo.HeaderXRequestID)
+			if id == "" {
+				id = res.Header().Get(echo.HeaderXRequestID)
+			}
+			fields = append(fields, zap.String("request_id", id))
+
+			// Append custom logger fields if provided
+			if config.CustomFieldFunc != nil {
+				fields = append(fields, config.CustomFieldFunc(c)...)
+			}
 
 			n := res.Status
 			switch {
--- logger_test.go
+++ logger_test.go
@@ -1,8 +1,11 @@
 package echozap
 
 import (
+	"github.com/uptrace/opentelemetry-go-extra/otelzap"
+	"go.uber.org/zap/zapcore"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/labstack/echo/v4"
@@ -37,4 +40,128 @@ func TestZapLogger(t *testing.T) {
 	assert.Equal(t, "GET /something", logFields["request"])
 	assert.NotNil(t, logFields["host"])
 	assert.NotNil(t, logFields["size"])
+}
+
+func TestZapLoggerWithConfig(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/something", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	h := func(c echo.Context) error {
+		return c.String(http.StatusOK, "")
+	}
+
+	obs, logs := observer.New(zap.DebugLevel)
+
+	logger := zap.New(obs)
+
+	err := ZapLoggerWithConfig(logger, ZapLoggerConfig{
+		Skipper: func(ctx echo.Context) bool {
+			return strings.Contains(ctx.Request().URL.Path, "/something")
+		},
+	})(h)(c)
+
+	assert.Nil(t, err)
+
+	assert.Equal(t, 0, logs.Len())
+}
+
+func TestOtelZapLogger(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/something", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	h := func(c echo.Context) error {
+		return c.String(http.StatusOK, "")
+	}
+
+	obs, logs := observer.New(zap.DebugLevel)
+
+	logger := zap.New(obs)
+
+	otelLogger := otelzap.New(logger)
+
+	err := OtelZapLogger(otelLogger)(h)(c)
+
+	assert.Nil(t, err)
+
+	logFields := logs.AllUntimed()[0].ContextMap()
+
+	assert.Equal(t, 1, logs.Len())
+	assert.Equal(t, int64(200), logFields["status"])
+	assert.NotNil(t, logFields["latency"])
+	assert.Equal(t, "GET /something", logFields["request"])
+	assert.NotNil(t, logFields["host"])
+	assert.NotNil(t, logFields["size"])
+}
+
+func TestOtelZapLoggerWithConfig(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/something", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	h := func(c echo.Context) error {
+		return c.String(http.StatusOK, "")
+	}
+
+	obs, logs := observer.New(zap.DebugLevel)
+
+	logger := zap.New(obs)
+
+	otelLogger := otelzap.New(logger)
+
+	err := OtelZapLoggerWithConfig(otelLogger, ZapLoggerConfig{
+		Skipper: func(ctx echo.Context) bool {
+			return strings.Contains(ctx.Request().URL.Path, "/something")
+		},
+	})(h)(c)
+
+	assert.Nil(t, err)
+
+	assert.Equal(t, 0, logs.Len())
+}
+
+func TestOtelCustomLoggerFields(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/something", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	h := func(c echo.Context) error {
+		return c.String(http.StatusOK, "")
+	}
+
+	obs, logs := observer.New(zap.DebugLevel)
+
+	logger := zap.New(obs)
+
+	otelLogger := otelzap.New(logger)
+
+	// Define your custom logger fields generator function
+	customFieldFunc := func(c echo.Context) []zapcore.Field {
+		// Generate and return custom logger fields based on the context
+		// ...
+		return []zapcore.Field{
+			zap.String("custom_field1", "value1"),
+			zap.String("custom_field2", "value2"),
+		}
+	}
+
+	// Configure the ZapLogger middleware with custom fields
+	config := ZapLoggerConfig{
+		CustomFieldFunc: customFieldFunc,
+	}
+
+	err := OtelZapLoggerWithConfig(otelLogger, config)(h)(c)
+
+	assert.Nil(t, err)
+
+	logFields := logs.AllUntimed()[0].ContextMap()
+
+	assert.Equal(t, 1, logs.Len())
+	assert.Equal(t, "value1", logFields["custom_field1"])
+	assert.NotNil(t, logFields["custom_field2"])
 }
